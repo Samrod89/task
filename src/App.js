9acce90726b7023960c3da5566a2844ed2578689
Samrod89 <68875830+Samrod89@users.noreply.github.com>
2021-02-25 22:22:28 -0500

    fist

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ToDoContainer from './components/ToDoContainer'
+
+function App() {
+  /* Mienbros del Squad:
+    Jorge araque, Sandra Rodriguez, Paul Alberto Briseño*/
+
+  return (
+    <ToDoContainer/>
+  );
+}
+
+export default App;
+
